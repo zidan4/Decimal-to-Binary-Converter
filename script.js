54f8b4640c0a04e57a41f7e1964d0c356fd731c0
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2024-08-23 21:42:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
+
 const result = document.getElementById("result");
 const animationContainer = document.getElementById("animation-container");
 const animationData = [
